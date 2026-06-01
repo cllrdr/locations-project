7fe3fcff6f76e0aa02246e155324a84f5e431945
Alex Chernobrov <aachernobrov@yandex.ru>
2026-06-01 12:49:53 +0300

    user_session_redis v4 clean

--- build/seed.sql
+++ build/seed.sql
@@ -4,11 +4,11 @@ SELECT pg_reload_conf();
 
 -- Локации
 INSERT INTO locations (name, description, short_description, image_path, video_path, players, is_deleted) VALUES
-('Тирсфальские леса', 'Заброшенные леса Лордерона с богатыми залежами золота и лесными угодьями.', 'Ancient forest with gold deposits', 'http://localhost:9000/locations/map1.png', 'http://localhost:9000/locations/map1.mp4', '3-6', FALSE),
-('Пустоши Дурхота', 'Сухие красные степи, где золото встречается часто, а дерево можно найти только в редких оазисах.', 'Red steppes and oases', 'http://localhost:9000/locations/map2.png', 'http://localhost:9000/locations/map2.mp4', '2-4', FALSE),
-('Ледяная Корона', 'Суровая мерзлота с незначительными запасами древесины, но богатыми золотыми жилами глубоко во льдах.', 'Icy lands with golden veins', 'http://localhost:9000/locations/map3.png', 'http://localhost:9000/locations/map3.mp4', '4-8', FALSE),
-('Болота Печали', 'Топкое негостеприимное место, бедное на любые ресурсы — только выживание и контроль над ограниченными источниками.', 'Dangerous swamps', 'http://localhost:9000/locations/map4.png', 'http://localhost:9000/locations/map4.mp4', '1-3', FALSE),
-('Пылающие степи', 'Выжженная демоническая земля, где нет деревьев, но золото течет рекой (в прямом смысле — жидкое золото в лаве).', 'Demonic lava fields', 'http://localhost:9000/locations/map5.png', 'http://localhost:9000/locations/map5.mp4', '2-4', FALSE);
+('Тирсфальские леса', 'Заброшенные леса Лордерона с богатыми залежами золота и лесными угодьями.', 'Ancient forest with gold deposits', 'http://localhost:9000/locations/map1.jpg', 'http://localhost:9000/locations/map1.mp4', '3-6', FALSE),
+('Пустоши Дурхота', 'Сухие красные степи, где золото встречается часто, а дерево можно найти только в редких оазисах.', 'Red steppes and oases', 'http://localhost:9000/locations/map2.jpg', 'http://localhost:9000/locations/map2.mp4', '2-4', FALSE),
+('Ледяная Корона', 'Суровая мерзлота с незначительными запасами древесины, но богатыми золотыми жилами глубоко во льдах.', 'Icy lands with golden veins', 'http://localhost:9000/locations/map3.jpg', 'http://localhost:9000/locations/map3.mp4', '4-8', FALSE),
+('Болота Печали', 'Топкое негостеприимное место, бедное на любые ресурсы — только выживание и контроль над ограниченными источниками.', 'Dangerous swamps', 'http://localhost:9000/locations/map4.jpg', 'http://localhost:9000/locations/map4.mp4', '1-3', FALSE),
+('Пылающие степи', 'Выжженная демоническая земля, где нет деревьев, но золото течет рекой (в прямом смысле — жидкое золото в лаве).', 'Demonic lava fields', 'http://localhost:9000/locations/map5.jpg', 'http://localhost:9000/locations/map5.mp4', '2-4', FALSE);
 
 -- Пользователи
 INSERT INTO users (email, name, password, is_moderator) VALUES
